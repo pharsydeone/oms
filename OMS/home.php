9890270f6a84308f89226b0d893fe7a62a5d686d
ab <ab@me.com>
2014-07-18 00:59:30 -0500

    midnight

--- OMS/home.php
+++ OMS/home.php
@@ -8,19 +8,21 @@ Template Name: Home Page
 
 <div id="container" class="cf" style="background-image:url(<?php the_field('background_image'); ?>)">	
 		<div id="content">
-			<div class="home-spacer"></div>
-			<div id="content-copy">
-				<div class="home-title"><?php the_field('home_title'); ?></div>
-				<div class="home-sub-title"><?php the_field('home_subtitle'); ?></div>
-				<div class="home-copy">
-					<?php if (have_posts()) : while (have_posts()) : the_post();?>
-						<?php the_content(); ?>
-					<?php endwhile; endif; ?>
-				</div>
-					   
-				<p>&nbsp;</p>
-		  </div>
-		</div>
+            <div id="content-copy-wrapper">
+            <a href="#" id="formDrop">Snoo</a>
+                <div id="content-copy">
+                    <div class="home-title"><?php the_field('home_title'); ?></div>
+                    <div class="home-sub-title"><?php the_field('home_subtitle'); ?></div>
+                    <div class="home-copy">
+                        <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                            <?php the_content(); ?>
+                        <?php endwhile; endif; ?>
+                    </div>
+                           
+                    <p>&nbsp;</p>
+              </div>
+            </div>
+        </div>
 </div>
 
 <?php get_footer(); ?>
--- OMS/index.php
+++ OMS/index.php
@@ -1,46 +1,49 @@
 <?php get_header(); ?>
 
 <div id="container" class="cf" style="background-image:url(<?php the_field('background_image'); ?>)">	
-	<div class="arrow-left"><?php echo ruab_prev_page_link(); ?></div>
-	<div class="arrow-right"><?php echo ruab_next_page_link(); ?></div>
 	<div id="content">
-		<div id="content-copy">
-			<div id="page-nav-wrapper" class="cf">
-				<header>
-                	<div class="mobile-title"><?php wp_title("", true); ?></div>
-					<nav id="page-nav">
-						<span id="page-title"><?php wp_title("", true); ?></span>
-						<?php
-						  if (!is_home()) {
-							  if($post->post_parent) {
-								$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-								$var1 = '<a';
-								$var2 = '<span class="page-nav-item"><a';
-								$var3 = '</a>';
-								$var4 = '</a></span>';
-								$children = str_replace($var1, $var2, $children);
-								$children = str_replace($var3, $var4, $children);									
-							  } else {
-								$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-								$var1 = '<a';
-								$var2 = '<span class="page-nav-item"><a';
-								$var3 = '</a>';
-								$var4 = '</a></span>';
-								$children = str_replace($var1, $var2, $children);
-								$children = str_replace($var3, $var4, $children);
-							  }
-							  if ($children) { ?>
-						  <ul id="page-nav-wrapper-wrapper">
-						  <?php echo $children; ?>
-						  </ul>
-						<?php }} ?>
-					</nav>
-				</header>
-			</div>
-			<?php if (have_posts()) : while (have_posts()) : the_post();?>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?>
-		</div>
+    <div class="arrow-left"><?php echo ruab_prev_page_link(); ?></div>
+	<div class="arrow-right"><?php echo ruab_next_page_link(); ?></div>
+		<div id="content-copy-wrapper">
+        <a href="#" id="formDrop">Snoo</a>
+            <div id="content-copy">                
+                <div id="page-nav-wrapper" class="cf">
+                    <header>
+                        <div class="mobile-title"><?php wp_title("", true); ?></div>
+                        <nav id="page-nav">
+                            <span id="page-title"><?php wp_title("", true); ?></span>
+                            <?php
+                              if (!is_home()) {
+                                  if($post->post_parent) {
+                                    $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+                                    $var1 = '<a';
+                                    $var2 = '<span class="page-nav-item"><a';
+                                    $var3 = '</a>';
+                                    $var4 = '</a></span>';
+                                    $children = str_replace($var1, $var2, $children);
+                                    $children = str_replace($var3, $var4, $children);									
+                                  } else {
+                                    $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+                                    $var1 = '<a';
+                                    $var2 = '<span class="page-nav-item"><a';
+                                    $var3 = '</a>';
+                                    $var4 = '</a></span>';
+                                    $children = str_replace($var1, $var2, $children);
+                                    $children = str_replace($var3, $var4, $children);
+                                  }
+                                  if ($children) { ?>
+                              <ul id="page-nav-wrapper-wrapper">
+                              <?php echo $children; ?>
+                              </ul>
+                            <?php }} ?>
+                        </nav>
+                    </header>
+                </div>
+                <?php if (have_posts()) : while (have_posts()) : the_post();?>
+                    <?php the_content(); ?>
+                <?php endwhile; endif; ?>
+            </div>
+        </div>
 	</div>
 </div>
 
